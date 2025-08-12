e7d72cdf9dded727b2f1fee1bec13d1fa1572439
DLizanaDev <Helgait@usal.es>
2025-08-12 13:15:04 +0200

    Introduction to variables in javascript

---
+++ variables.js
@@ -0,0 +1,17 @@
+
+// Para variables añadir var
+var nombre = "Joe";
+
+var numero = 32;
+
+// Un pop-up
+alert(nombre);
+
+// Un pop-up en ventana
+window.alert(numero);
+
+//Similar a input en python, para guardar info en la variable
+var tuNombre = prompt("¿Cómo te llamas?");
+
+// Para saber el tipo de dato que devuelve una variable
+typeof(nombre);
