b5def77584ab6bb845d928537ebf1a3bdcbb24ff
DLizanaDev <Helgait@usal.es>
2025-08-12 14:02:14 +0200

    Basic variables and methods

--- variables.js
+++ variables.js
@@ -15,3 +15,28 @@ var tuNombre = prompt("¿Cómo te llamas?");
 
 // Para saber el tipo de dato que devuelve una variable
 typeof(nombre);
+
+// Contatenación de Strings (Igual que en Java)
+alert("Hola "+"mundo.");
+alert("Hola "+nombre);
+
+// Contar caracteres del String con .length
+var numeroLetras = nombre.length;
+
+
+// Slice de Strings para extraer partes concretas
+// Selecciona la informacion entre los dos puntos seleccionados
+nombre.slice(0,1);
+var frase = "aasdkjaskjdfalaksdfjh";
+var fraseMaxima = slice.frase(0,10);
+
+nombre.toUpperCase();
+nombre.toLowerCase();
+
+var x = 5;
+var y = 1;
+
+x++;
+x--;
+
+x += y;
