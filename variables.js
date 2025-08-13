dc5490893fc6918869c2cf83bd1347c039ca2ec6
DLizanaDev <Helgait@usal.es>
2025-08-13 11:41:54 +0200

    Added files for drumsExercise

--- variables.js
+++ variables.js
@@ -43,3 +43,18 @@ x++;
 x--;
 
 x += y;
+
+
+// swtich case como en java:
+
+switch(x){
+    case 0:
+        console.log(0);
+        break;
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+}
