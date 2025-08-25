ea29527f27a96349a6b2473f231142c6dbc963fb
DLizanaDev <Helgait@usal.es>
2025-08-25 20:17:46 +0200

    Convert Length function

--- ejerciciosFunciones.js
+++ ejerciciosFunciones.js
@@ -1,5 +1,5 @@
 
-
+//  Ejercicio 1
 function greet(name){
     if(!name){
         console.log('Hi there!')
@@ -11,7 +11,7 @@ function greet(name){
 
 greet('Pepe');
 
-
+// Ejercicio 2
 function convertToFahrenheit(celsius){
     let result = (celsius * 9/5)+32;
     return result;
@@ -20,7 +20,7 @@ function convertToFahrenheit(celsius){
 convertToFahrenheit(36.5);
 
 
-
+// Ejercicio 3
 function convertToCelsius(fahrenheit){
     let result = (fahrenheit -32)* 5/9;
     return result;
@@ -28,16 +28,51 @@ function convertToCelsius(fahrenheit){
 
 convertToCelsius(100);
 
-
+// Ejercicio 4
 function convertTemperature(degrees,unit){
     if(unit === 'C'){
-        convertToFahrenheit(degrees);
+        return `${convertToFahrenheit(degrees)}`;
     }
     else if(unit === 'F'){
-        convertToCelsius(degrees);
+        return `${convertToCelsius(degrees)}`;
     }
     else{
         console.log('Incorrect unit, select C or F')
     }
 } 
 
+
+
+// Ejercicio 5
+
+function kmToMiles(km){
+    return km/1.6
+}
+
+function milesToKm(miles){
+    return miles*1.6
+}
+
+
+function convertLength(length,from,to){
+    if(from === 'miles'){
+        if(to === 'km'){
+            return milesToKm(length);
+        }
+        else if(to === 'miles'){
+            return length;
+        }
+    }
+    else if(from === 'km'){
+        if(to === 'miles'){
+            return kmToMiles(length);
+        }
+        else if (to === 'km'){
+            return length;
+        }
+    }
+    else{
+        return `Invalid unit: ${from}`;
+    }
+    
+}
