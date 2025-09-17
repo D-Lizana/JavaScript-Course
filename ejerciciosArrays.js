02707edac145a154d5440f896f39201fff43a1bd
DLizanaDev <Helgait@usal.es>
2025-09-17 12:23:00 +0200

    Solved arrays exercise 1

---
+++ ejerciciosArrays.js
@@ -0,0 +1,14 @@
+//Ejercicio 1
+
+let palabras = ['good','search','pepper','gallina'];
+
+for(let i=0; i<palabras.length;i++){
+
+    if(palabras[i] === 'search'){
+        console.log(i);
+    }
+    else{
+        console.log(-1);
+    }
+
+}
